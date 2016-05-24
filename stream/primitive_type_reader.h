c506d8c751d9f995e54e23e2d8763937337ec668
Michał Łazowik <mlazowik@gmail.com>
2016-05-24 13:24:31 +0200

    Fix template initialization

--- stream/primitive_type_reader.h
+++ stream/primitive_type_reader.h
@@ -23,4 +23,37 @@ private:
     StreamReader *streamReader;
 };
 
+template <typename T>
+PrimitiveTypeReader<T>::PrimitiveTypeReader(Stream &stream) : stream(stream) {
+    this->value = new T;
+    this->streamReader = new StreamReader(stream, (char*)this->value,
+                                          sizeof(*this->value));
+}
+
+template <typename T>
+PrimitiveTypeReader<T>::~PrimitiveTypeReader() {
+    delete this->value;
+    delete this->streamReader;
+}
+
+template <typename T>
+void PrimitiveTypeReader<T>::readNextChunk() {
+    this->streamReader->readNextChunk();
+}
+
+template <typename T>
+bool PrimitiveTypeReader<T>::finished() const {
+    return this->streamReader->finished();
+}
+
+template <typename T>
+T PrimitiveTypeReader<T>::getValue() const {
+    if (!this->finished()) {
+        throw std::logic_error("cannot return value, still reading");
+    }
+
+    T value = *this->value;
+    return value;
+}
+
 #endif //CHAT_PRIMITIVE_TYPE_READER_H
